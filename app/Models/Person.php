34c976cd61669b883aaed68a33bed3993f6bf9ac
xinnaider <fernandoschnneider@gmail.com>
2024-08-31 00:28:15 -0300

    fix: nome da tabela incorreto e erros bobos

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model
 {
-    protected $table = 'person';
+    protected $table = 'persons';
     protected $guarded = [];
 
     public function user()
--- app/Models/User.php
+++ app/Models/User.php
@@ -1,16 +1,14 @@
 <?php
-
 namespace App\Models;
-
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, HasApiTokens;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -21,6 +19,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
+        'person_id'
     ];
 
     /**
@@ -31,18 +31,29 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'person_id'
     ];
 
     /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
+     * 
+     * The relationships that should always be loaded.
+     * 
+     * @var array<string>
      */
-    protected function casts(): array
+    protected $with = ['person'];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'role' => 'string'  
+    ];
+
+    public function person(): HasOne
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->hasOne(Person::class, 'id', 'person_id');
     }
 }
