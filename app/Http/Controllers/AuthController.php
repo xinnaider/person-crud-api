68805a7818f66b4719e4503ac5d1779449d4eb43
xinnaider <fernandoschnneider@gmail.com>
2024-08-31 00:27:26 -0300

    feat: login e registro

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,35 +2,89 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use App\Models\Person;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends ApiController
 {
     public function login(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->error($validator->errors(), 400);
+        }
+
         $credentials = $request->only('email', 'password');
 
         if (!Auth::attempt($credentials)) {
-            return $this->error('Invalid credentials', 401);
+            return $this->error('Unauthorized', 401);
         }
 
-        $request->user()->tokens()->delete();
+        $token = app('App\Services\TokenService')->generateToken($request->user());
 
-        $token = $request->user()->createToken('auth_token')->plainTextToken;
+        return $this->success(data: [
+            'token_type' => 'bearer',
+            'token' => $token->plainTextToken,
+        ], message: 'User logged in');
+    }
 
-        return $this->success(data: ['token' => $token], message: 'User logged in');
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'title' => 'required',
+            'birth_date' => 'required|date|before:today|date_format:Y-m-d',
+            'relationship' => 'required|in:single,married,divorced,widowed',
+            'email' => 'required|email|unique:users',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->error($validator->errors(), 400);
+        }
+
+        $validated = $validator->validated();
+
+        $person = Person::create([
+            'name' => $validated['name'],
+            'title' => $validated['title'],
+            'birth_date' => $validated['birth_date'],
+            'relationship' => $validated['relationship']
+        ]);
+
+        $user = User::create([
+            'email' => $validated['email'],
+            'password' => bcrypt($validated['password']),
+            'role' => 'user',
+            'person_id' => $person->id
+        ]);
+
+        $token = app('App\Services\TokenService')->generateToken($user);
+
+        return $this->success(data: [
+            'user' => $user->load('person')->toArray(),
+            'token_type' => 'bearer',
+            'token' => $token->plainTextToken,
+        ], message: 'User registered');
     }
 
     public function user(Request $request)
     {
-        return $this->success($request->user());
+        $user = $request->user();
+
+        return $this->success($user);
     }
 
     public function logout(Request $request)
     {
-        $request->user()->tokens()->delete();
+        app('App\Services\TokenService')->revokeTokens($request->user());
 
         return $this->success(message: 'User logged out');
     }
--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PersonController;
 
 Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', [AuthController::class, 'user'])->name('user');
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::controller(AuthController::class)->group(function () {
+        Route::get('/user', 'user');
+        Route::post('/logout', 'logout');
+    });
+
+    Route::apiResource('person', PersonController::class);
 });
